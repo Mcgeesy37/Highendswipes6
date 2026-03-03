7a24b9bba12bd40d2c576ade4260df29e387b815
Mcgeesy37 <98265082+Mcgeesy37@users.noreply.github.com>
2026-03-03 23:25:48 +0100

    Update globe.js

--- assets/js/globe.js
+++ assets/js/globe.js
@@ -19,6 +19,10 @@ window.innerHeight
 document.getElementById("globe")
 .appendChild(renderer.domElement)
 
+camera.position.z = 10
+
+
+/* GLOBE */
 
 const geometry = new THREE.SphereGeometry(5,64,64)
 
@@ -38,15 +42,50 @@ material
 
 scene.add(globe)
 
-camera.position.z = 10
+
+/* NETWORK POINTS */
+
+const pointsGeometry = new THREE.BufferGeometry()
+
+const pointsCount = 400
+
+const positions = new Float32Array(pointsCount*3)
+
+for(let i=0;i<pointsCount;i++){
+
+positions[i*3] = (Math.random()-0.5)*12
+positions[i*3+1] = (Math.random()-0.5)*12
+positions[i*3+2] = (Math.random()-0.5)*12
+
+}
+
+pointsGeometry.setAttribute(
+"position",
+new THREE.BufferAttribute(positions,3)
+)
+
+const pointsMaterial = new THREE.PointsMaterial({
+
+color:0xc6a76a,
+size:0.03
+
+})
+
+const points = new THREE.Points(
+pointsGeometry,
+pointsMaterial
+)
+
+scene.add(points)
 
 
 function animate(){
 
 requestAnimationFrame(animate)
 
-globe.rotation.y += 0.002
-globe.rotation.x += 0.0005
+globe.rotation.y += 0.0015
+
+points.rotation.y += 0.0008
 
 renderer.render(scene,camera)
 
